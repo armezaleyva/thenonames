a3e6a934e29cb244f97d15299d245b4c085b1dea
Armando Meza Leyva <42181268+armezaleyva@users.noreply.github.com>
2019-10-23 21:02:17 -0700

    Almost done with planting scene

--- Assets/Scripts/Missions/Putflower.cs
+++ Assets/Scripts/Missions/Putflower.cs
@@ -6,13 +6,34 @@ using UnityEngine.SceneManagement;
 public class Putflower : MonoBehaviour
 {
     public GameObject flower;
+    public GameObject terrain1;
+    public GameObject terrain2;
+    public GameObject terrain3;
+    public GameObject terrain4;
+
+    private bool canContinue = true;
+
     public void Putflor()
     {
-        
-
         if (flower.tag == "cubo")
         {
+            StartCoroutine(TransitionTerrains());
             //SceneManager.LoadScene("Scene6");
         }
+    }
+
+    IEnumerator TransitionTerrains() {
+        yield return StartCoroutine(LoadTerrain(terrain1, terrain2));
+        yield return StartCoroutine(LoadTerrain(terrain2, terrain3));
+        yield return StartCoroutine(LoadTerrain(terrain3, terrain4));
+    }
+
+
+    IEnumerator LoadTerrain(GameObject oldTerrain, GameObject newTerrain) {
+        oldTerrain.SetActive(false);
+        newTerrain.SetActive(true);
+        canContinue = false;
+
+        yield return new WaitForSeconds(5);
     }
 }
